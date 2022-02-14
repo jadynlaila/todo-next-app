77521bf248e7c4a7db6bf93cf0336f1df7c75304
Jadyn Calhoun <Jcalho217@west-mec.local>
2022-02-14 15:30:16 -0700

    all files

--- pages/sort.jsx
+++ pages/sort.jsx
@@ -1,17 +1,40 @@
-import React from "react";
 import { Message } from "semantic-ui-react";
+import { useState, useEffect } from "react";
 
+
+
+//!need to remove export from before all the functions and instead use module.export
 const sort = () => {
   const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const names =[brandon]
 
   useEffect(async () => {
     const res = await axios.get("https://jsonplaceholder.typicode.com/todos");
     setPosts(res.data);
   }, []);
-}
-  export const user1 = () => {
+
+  const user1 = () => {
+    return (
+      <Message>
+        <h1>Amy's To-Do List</h1>
+        {/* {posts.map((post) => {
+        const { title, id, completed } = posts;
+        if (completed) {
+          return (
+            <Message key={id} color={completed ? { red } : { green }}>
+              <div>{title}</div>
+            </Message>
+          );
+        }
+      })} */}
+      </Message>
+    );
+  };
+
+  const user2 = () => {
     <Message>
-      <h1>Amy's To-Do List</h1>
+      <h1>{`${names[userId-1]}'s To-Do List`}</h1>
       {posts.map((post) => {
         const { title, id, completed } = posts;
         if (completed) {
@@ -24,142 +47,126 @@ const sort = () => {
       })}
     </Message>;
   };
+  const user3 = () => {
+    <Message>
+      <h1>Chey's To-Do List</h1>
+      {posts.map((post) => {
+        const { title, id, completed } = posts;
+        if (completed) {
+          return (
+            <Message key={id} color={completed ? { red } : { green }}>
+              <div>{title}</div>
+            </Message>
+          );
+        }
+      })}
+    </Message>;
+  };
+  const user4 = () => {
+    <Message>
+      <h1>Brandon's To-Do List</h1>
+      {posts.map((post) => {
+        const { title, id, completed } = posts;
+        if (completed) {
+          return (
+            <Message key={id} color={completed ? { red } : { green }}>
+              <div>{title}</div>
+            </Message>
+          );
+        }
+      })}
+    </Message>;
+  };
+  const user5 = () => {
+    <Message>
+      <h1>Brandon's To-Do List</h1>
+      {posts.map((post) => {
+        const { title, id, completed } = posts;
+        if (completed) {
+          return (
+            <Message key={id} color={completed ? { red } : { green }}>
+              <div>{title}</div>
+            </Message>
+          );
+        }
+      })}
+    </Message>;
+  };
+  const user6 = () => {
+    <Message>
+      <h1>Brandon's To-Do List</h1>
+      {posts.map((post) => {
+        const { title, id, completed } = posts;
+        if (completed) {
+          return (
+            <Message key={id} color={completed ? { red } : { green }}>
+              <div>{title}</div>
+            </Message>
+          );
+        }
+      })}
+    </Message>;
+  };
+  const user7 = () => {
+    <Message>
+      <h1>Brandon's To-Do List</h1>
+      {posts.map((post) => {
+        const { title, id, completed } = posts;
+        if (completed) {
+          return (
+            <Message key={id} color={completed ? { red } : { green }}>
+              <div>{title}</div>
+            </Message>
+          );
+        }
+      })}
+    </Message>;
+  };
+  const user8 = () => {
+    <Message>
+      <h1>Brandon's To-Do List</h1>
+      {posts.map((post) => {
+        const { title, id, completed } = posts;
+        if (completed) {
+          return (
+            <Message key={id} color={completed ? { red } : { green }}>
+              <div>{title}</div>
+            </Message>
+          );
+        }
+      })}
+    </Message>;
+  };
+  const user9 = () => {
+    <Message>
+      <h1>Brandon's To-Do List</h1>
+      {posts.map((post) => {
+        const { title, id, completed } = posts;
+        if (completed) {
+          return (
+            <Message key={id} color={completed ? { red } : { green }}>
+              <div>{title}</div>
+            </Message>
+          );
+        }
+      })}
+    </Message>;
+  };
+  const user10 = () => {
+    <Message>
+      <h1>Brandon's To-Do List</h1>
+      {posts.map((post) => {
+        const { title, id, completed } = posts;
+        if (completed) {
+          return (
+            <Message key={id} color={completed ? { red } : { green }}>
+              <div>{title}</div>
+            </Message>
+          );
+        }
+      })}
+    </Message>;
+  };
+};
 
-  export const user2 = () => {
-    <Message>
-    <h1>Brandon's To-Do List</h1>
-    {posts.map((post) => {
-      const { title, id, completed } = posts;
-      if (completed) {
-        return (
-          <Message key={id} color={completed ? { red } : { green }}>
-            <div>{title}</div>
-          </Message>
-        );
-      }
-    })}
-  </Message>;
-  };
-  export const user3 = () => {
-    <Message>
-    <h1>Chey's To-Do List</h1>
-    {posts.map((post) => {
-      const { title, id, completed } = posts;
-      if (completed) {
-        return (
-          <Message key={id} color={completed ? { red } : { green }}>
-            <div>{title}</div>
-          </Message>
-        );
-      }
-    })}
-  </Message>;
-  };
-  export const user4 = () => {
-    <Message>
-    <h1>Brandon's To-Do List</h1>
-    {posts.map((post) => {
-      const { title, id, completed } = posts;
-      if (completed) {
-        return (
-          <Message key={id} color={completed ? { red } : { green }}>
-            <div>{title}</div>
-          </Message>
-        );
-      }
-    })}
-  </Message>;
-  };
-  export const user5 = () => {
-    <Message>
-    <h1>Brandon's To-Do List</h1>
-    {posts.map((post) => {
-      const { title, id, completed } = posts;
-      if (completed) {
-        return (
-          <Message key={id} color={completed ? { red } : { green }}>
-            <div>{title}</div>
-          </Message>
-        );
-      }
-    })}
-  </Message>;
-  };
-  export const user6 = () => {
-    <Message>
-    <h1>Brandon's To-Do List</h1>
-    {posts.map((post) => {
-      const { title, id, completed } = posts;
-      if (completed) {
-        return (
-          <Message key={id} color={completed ? { red } : { green }}>
-            <div>{title}</div>
-          </Message>
-        );
-      }
-    })}
-  </Message>;
-  };
-  export const user7 = () => {
-    <Message>
-    <h1>Brandon's To-Do List</h1>
-    {posts.map((post) => {
-      const { title, id, completed } = posts;
-      if (completed) {
-        return (
-          <Message key={id} color={completed ? { red } : { green }}>
-            <div>{title}</div>
-          </Message>
-        );
-      }
-    })}
-  </Message>;
-  };
-  export const user8 = () => {
-    <Message>
-    <h1>Brandon's To-Do List</h1>
-    {posts.map((post) => {
-      const { title, id, completed } = posts;
-      if (completed) {
-        return (
-          <Message key={id} color={completed ? { red } : { green }}>
-            <div>{title}</div>
-          </Message>
-        );
-      }
-    })}
-  </Message>;
-  };
-  export const user9 = () => {
-    <Message>
-    <h1>Brandon's To-Do List</h1>
-    {posts.map((post) => {
-      const { title, id, completed } = posts;
-      if (completed) {
-        return (
-          <Message key={id} color={completed ? { red } : { green }}>
-            <div>{title}</div>
-          </Message>
-        );
-      }
-    })}
-  </Message>;
-  };
-  export const user10 = () => {
-    <Message>
-    <h1>Brandon's To-Do List</h1>
-    {posts.map((post) => {
-      const { title, id, completed } = posts;
-      if (completed) {
-        return (
-          <Message key={id} color={completed ? { red } : { green }}>
-            <div>{title}</div>
-          </Message>
-        );
-      }
-    })}
-  </Message>;
-  };
-
-
-export default sort;
+module.exports = {user1, user2, user3}
